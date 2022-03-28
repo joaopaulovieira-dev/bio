e0fbc542fab75406ba1fa7917807f483fa529a8d
João Paulo Vieira <contato@joaopaulovieira.dev>
2022-03-28 15:43:40 -0300

    Add Home.

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:paylist/modules/login/cadastro_page.dart';
-
 import 'modules/home/home_page.dart';
-import 'modules/splash/splash_page.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
@@ -11,16 +8,16 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PayList.',
-      initialRoute: '/splash',
+      title: 'Bio - João Paulo',
+      initialRoute: '/home',
       routes: {
-        '/splash': (context) => const SplashPage(),
-        '/cadastro': (context) => const CadastroPage(),
+        "/home": (context) => const HomePage(),
+        //'/splash': (context) => const SplashPage(),
+        //'/cadastro': (context) => const CadastroPage(),
         // "/home": (context) => HomePage(
         //       user: ModalRoute.of(context)!.settings.arguments as UserModel,
         //     ), //carrega o contexto do usuário
-        "/home": (context) => const HomePage(),
-        // '/error': (context) => ErrorPage(),
+        //'/error': (context) => ErrorPage(),
       },
       theme: ThemeData(
         textTheme: GoogleFonts.interTextTheme(),
